4bd909ad1a238df876292227b2a8038a626efd3e
Jeff Haynie <jhaynie@appcelerator.com>
2014-05-25 23:30:20 -0700

    added mocha tests

---
+++ tests/a/app.js
@@ -0,0 +1,14 @@
+"use hyperloop"
+
+var frame = CGRectMake(100,100,20,20);
+var window = new UIWindow();
+var view = new UIView(frame);
+view.backgroundColor = UIColor.blueColor();
+window.addSubview(view);
+window.makeKeyAndVisible();
+
+describe("UIView", function(){
+       it("should have backgroundColor of blueColor", function(){
+               String(view.backgroundColor).should.equal('UIDeviceRGBColorSpace 0 0 1 1');
+       });
+});
---
+++ tests/hooks/mocha.js
@@ -0,0 +1,69 @@
+/**
+ * simple hook that inject mocha
+ */
+var hl = require('hyperloopcommon'),
+       path = require('path'),
+       fs = require('fs'),
+       log = hl.log;
+
+exports.version = '>0.0.0';
+
+function createSourceEntry(data, fn, relative, name) {
+       if (!(fn in data.uncompiledSources)) {
+               var filename = path.relative(relative,fn) || 'index.js',
+                       entry = {
+                               source: fs.readFileSync(fn).toString(),
+                               json: path.extname(fn)==='.json',
+                               root: true,
+                               relative: '/node_modules/'+name+'/'+filename,
+                       };
+               data.uncompiledSources[fn] = entry;
+       }
+       return true;
+}
+
+function addUncompiledSource(data, fn, relative, name, filter) {
+       if (fs.existsSync(fn)) {
+               if (!hl.util.isDirectory(fn)) {
+                       return createSourceEntry(data,fn,relative,name);
+               }
+               var files = hl.util.filelisting(fn, filter);
+               for (var c=0;c<files.length;c++) {
+                       var file = files[c];
+                       if (/\.js(on)?$/.test(file)) {
+                               createSourceEntry(data, file, relative, name);
+                       }
+                       else if (hl.util.isDirectory(file)) {
+                               addUncompiledSource(data, file, relative, name, filter);
+                       }
+               }
+       }
+       return false;
+}
+
+function addRequire(source, line) {
+       var lines = source.split(/\n/);
+       line = /;$/.test(line) ? line : line+';';
+       if (lines[0].trim()==='"use hyperloop"') {
+               lines.splice(1,0,line);
+       }
+       else {
+               lines.unshift(line);
+       }
+       return lines.join('\n');
+}
+
+exports.init = function(cli) {
+       cli.on('compile.pre.compile.source', {
+               pre: function(data) {
+                       if (/describe\s*\(/.test(data.source)) {
+                               data.source = addRequire(data.source,'require("timocha");var should=require("should")');
+                               data.source += ';mocha.run();';
+                               var tiMochaFile = path.join(hl.dirname,'examples','mocha','timocha.js');
+                               addUncompiledSource(data,tiMochaFile,tiMochaFile,'timocha');
+                               var shouldFile = path.join(hl.dirname,'examples','mocha','should.js');
+                               addUncompiledSource(data,shouldFile,shouldFile,'should');
+                       }
+               }
+       });
+};
