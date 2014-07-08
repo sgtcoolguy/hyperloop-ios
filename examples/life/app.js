803aa8f292abe6d888ce6d641204c3c8281c1ec5
Kota Iguchi <developer@infosia.co.jp>
2014-07-08 18:19:09 +0900

    Working game of life example

--- examples/life/app.js
+++ examples/life/app.js
@@ -1,16 +1,7 @@
 "use hyperloop"
 
-var CELL_SIZE, FPS_INTERVAL;
-if (typeof HYPERLOOP_XSMALL !== 'undefined') {
-	CELL_SIZE = 1;
-	FPS_INTERVAL = 10;
-} else if (typeof HYPERLOOP_SMALL !== 'undefined') {
-	CELL_SIZE = 4;
+var CELL_SIZE = 4,
 	FPS_INTERVAL = 60;
-} else {
-	CELL_SIZE = 20;
-	FPS_INTERVAL = 60;
-}
 
 const screenSize = UIScreen.mainScreen().bounds.size,
 	height = screenSize.height,
@@ -18,11 +9,8 @@ const screenSize = UIScreen.mainScreen().bounds.size,
 	xSize = width / CELL_SIZE,
 	ySize = height / CELL_SIZE;
 
-// shorthand for using NSLog
-var __ = NSString.stringWithUTF8String;
-var console = {
-	log: function(s) { NSLog(__('%@'), s); },
-	debug: function(s) { DEBUG && console.log(s); }
+String.prototype.toUTF8 = function() {
+	return NSString.stringWithUTF8String('' + this);
 };
 
 // output raw dimensions
@@ -30,36 +18,37 @@ console.log(width + ' x ' + height);
 
 // TODO: move these definitions to their own modules
 // set the FPS and rendering functions based on CLI args
-var getFPS, render, lastTime;
+var getFPS, render, lastTime = NSDate.date();
 if (typeof HYPERLOOP_LINK !== 'undefined') {
 	lastTime = 0;
-	getFPS = function(sender) {
-		var fps = 1.0 / (sender.timestamp - lastTime);
-		lastTime = sender.timestamp;
-		return fps;
-	};
-	render = function(callback) {
-		console.debug('>>>>> Using CADisplayLink for render loop <<<<<');
-
-		// create a display link, using the render loop update function
-		var displayLink = CADisplayLink.displayLinkWithTarget(callback, NSSelectorFromString('update:'));
-
-		// attach the display link to the main run loop
-		displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), NSDefaultRunLoopMode);
-	};
-} else {
-	lastTime = NSDate.date();
 	getFPS = function(sender) {
 		var fps = 1.0 / -lastTime.timeIntervalSinceNow();
 		lastTime = NSDate.date();
 		return fps;
 	};
 	render = function(callback) {
-		console.debug('>>>>> Using NSTimer for render loop <<<<<');
+		console.log('>>>>> Using CADisplayLink for render loop <<<<<');
+
+		// create a display link, using the render loop update function
+		var displayLink = CADisplayLink.displayLinkWithTarget(callback, NSSelectorFromString('update:'.toUTF8()));
+
+		// attach the display link to the main run loop
+		displayLink.addToRunLoop(NSRunLoop.mainRunLoop(), NSDefaultRunLoopMode);
+		global.displayLink = displayLink;
+	};
+} else {
+	getFPS = function(sender) {
+		var fps = 1.0 / -lastTime.timeIntervalSinceNow();
+		lastTime = NSDate.date();
+		return fps;
+	};
+	render = function(callback) {
+		console.log('>>>>> Using NSTimer for render loop <<<<<');
 
 		// launch timer with TimerCallback as its target/selector
 		var timer = NSTimer.scheduledTimerWithTimeInterval(
-			1.0/100.0, callback, NSSelectorFromString('update:'), null, true);
+			1.0/FPS_INTERVAL, callback, NSSelectorFromString('update:'.toUTF8()), null, true);
+		global.timer = timer;
 	};
 }
 
@@ -89,9 +78,8 @@ function getNextState(x, y, alive) {
 
 // the render function
 var ctr = 0;
-function update(params) {
+function update(sender) {
 	var x, y, cell;
-
 	// render current generation
 	for (x = 0; x < xSize; x++) {
 		for (y = 0; y < ySize; y++) {
@@ -99,7 +87,8 @@ function update(params) {
 
 			// minimze number of times we need to modify the proxy object
 			if (cell.alive !== cell.lastAlive) {
-				cell.proxy.setHidden(!cell.alive);
+				var proxy = cell.proxy.cast('UIView');
+				proxy.setHidden(!cell.alive);
 			}
 
 			// save the state
@@ -118,29 +107,32 @@ function update(params) {
 	// show the average FPS
 	if (!(++ctr % FPS_INTERVAL)) {
 		ctr = 1;
-		label.setText(__('FPS: ' + Math.round(FPS_INTERVAL*10*getFPS(params.sender))/10));
+		var text = 'FPS: ' + (Math.round(FPS_INTERVAL*getFPS())).toFixed(2);
+		label.cast('UILabel').setText(NSString.stringWithUTF8String(text));
 	}
 
 }
 // START UI CONSTRUCTION
 
 // configure main window
-var keyWindow = UIApplication.sharedApplication().keyWindow;
-keyWindow.backgroundColor = UIColor.blackColor();
+var bounds = UIScreen.mainScreen().bounds;
+var window = Hyperloop.method(UIWindow, 'initWithFrame:').call(bounds);
+window.backgroundColor = UIColor.blackColor();
 
 // seed the grid
 var cells = [];
+var white = UIColor.whiteColor();
 for (var x = 0; x < xSize; x++) {
-
 	cells[x] = [];
 	for (var y = 0; y < ySize; y++) {
 		// determine whether or not this cell is alive
 		var alive = Math.random() >= 0.5;
 
 		// create a native UIView
-		var cellProxy = new UIView();
-		cellProxy.frame = CGRectMake(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
-		cellProxy.backgroundColor = UIColor.whiteColor();
+		var frame = CGRectMake(x*CELL_SIZE, y*CELL_SIZE, CELL_SIZE, CELL_SIZE);
+		var cellProxy = Hyperloop.method(UIView, 'initWithFrame:').call(frame);
+
+		cellProxy.backgroundColor = white;
 		cellProxy.setHidden(!alive);
 
 		// save the cell
@@ -151,7 +143,7 @@ for (var x = 0; x < xSize; x++) {
 		};
 
 		// add the cell to the window
-		keyWindow.addSubview(cellProxy);
+		window.addSubview(cellProxy);
 	}
 }
 
@@ -161,8 +153,12 @@ label.frame = CGRectMake(0, 0, 100, 40);
 label.backgroundColor = UIColor.colorWithRed(180/255, 0, 0, 0.8);
 label.textColor = UIColor.whiteColor();
 label.textAlignment = NSTextAlignmentCenter;
-label.setText(__('FPS: '));
-keyWindow.addSubview(label);
+label.setText('FPS: '.toUTF8());
+window.addSubview(label);
+
+window.makeKeyAndVisible();
+
+global.window = window;
 
 // RENDER LOOP
 
@@ -170,12 +166,13 @@ keyWindow.addSubview(label);
 Hyperloop.defineClass(RenderCallback)
 	.method({
 		name: 'update',
-		returnType: 'void',
+		returns: 'void',
 		arguments: [{type:'id',name:'sender'}],
 		action: update
 	})
 	.build();
 
+global.renderCallback = new RenderCallback();
 
 // Let's do this
-render(new RenderCallback());
+render(global.renderCallback);
