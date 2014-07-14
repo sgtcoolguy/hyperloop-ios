c9db02999d33b82ef79636949a89e8f27e1c6150
sgtcoolguy <chris.a.williams@gmail.com>
2014-07-14 10:44:25 -0400

    port over distanceTraveled app. NOT WORKING. Current breaks during AST tranform.

---
+++ examples/distanceTraveled/app.js
@@ -0,0 +1,124 @@
+"use hyperloop"
+
+var TARGET_FPS = 60;
+
+/*
+ Utility.
+ */
+String.prototype.toUTF8 = function() {
+	return NSString.stringWithUTF8String('' + this);
+};
+
+/*
+ Create our simple UI.
+ */
+var win = UIApplication.sharedApplication().keyWindow,
+	label = new UILabel();
+label.textColor = UIColor.darkTextColor();
+label.frame = CGRectMake(20, 20, 280, 280);
+label.font = UIFont.systemFontOfSize(72);
+label.textAlignment = NSTextAlignmentCenter;
+label.text = 'Loading...'.toUTF8();
+label.lineBreakMode = NSLineBreakByTruncatingTail;
+label.numberOfLinesMode = 2;
+win.addSubview(label);
+
+/*
+ Distance calculation.
+ */
+var calculateManually = false,
+	lastLocation,
+	lastSpeed = 0,
+	totalTraveled = 0,
+	totalDisplayed = 0;
+
+function handleNewPosition(params) {
+	var locations = params.didUpdateLocations;
+	for (var i = 0, iL = locations.count(); i < iL; i++) {
+		var location = locations.objectAtIndex(i),
+			coordinate = location.coordinate;
+		if (lastLocation) {
+			if (calculateManually) {
+				var lat1 = lastLocation.latitude, lon1 = lastLocation.longitude,
+					lat2 = coordinate.latitude, lon2 = coordinate.longitude,
+					kmTraveled = 3963.0 * Math.acos(
+						Math.sin(lat1 / 57.2958) * Math.sin(lat2 / 57.2958)
+							+ Math.cos(lat1 / 57.2958) * Math.cos(lat2 / 57.2958)
+							* Math.cos(lon2 / 57.2958 - lon1 / 57.2958)
+					);
+				totalTraveled += kmTraveled * 3280.8399;
+			}
+			else {
+				totalTraveled += location.distanceFromLocation(lastLocation) * 3.28084;
+			}
+		}
+		lastSpeed = location.speed;
+		lastLocation = location;
+	}
+}
+
+/*
+ Update the displayed amount.
+ */
+Hyperloop.defineClass(TimerCallback)
+	.implements('NSObject')
+	.method({
+		name: 'update',
+		returns: 'void',
+		arguments: [ { type: 'id', name: 'sender' } ],
+		action: update
+	})
+	.build();
+
+function update() {
+	if (totalTraveled && Math.abs(totalTraveled - totalDisplayed) > 0.1) {
+		totalDisplayed += Math.max((totalTraveled - totalDisplayed) / TARGET_FPS, 0.1);
+		var mas = new NSMutableAttributedString(),
+			distance = (totalDisplayed | 0) + 'ft\n',
+			distanceRange = NSMakeRange(0, distance.length),
+			speed = (lastSpeed | 0) + 'mph',
+			speedRange = NSMakeRange(distance.length, speed.length),
+			iws = mas.initWithString((distance + speed).toUTF8());
+		
+		iws.addAttribute(kCTForegroundColorAttributeName, UIColor.redColor(), distanceRange);
+		iws.addAttribute(kCTForegroundColorAttributeName, UIColor.blueColor(), speedRange);
+		
+		label.attributedText = iws;
+	}
+}
+
+
+/*
+ Location manager hooks.
+ */
+var manager = new CLLocationManager();
+manager.purpose = 'To track how far you have traveled, of course!'.toUTF8();
+manager.distanceFilter = kCLDistanceFilterNone;
+manager.desiredAccuracy = kCLLocationAccuracyBest;
+Hyperloop.defineClass(LocDelegate)
+	.implements('NSObject')
+	.protocol('CLLocationManagerDelegate')
+	.method({
+		name: 'locationManager',
+		returns: 'void',
+		arguments: [
+			{ type: 'CLLocationManager', name: 'locationManager' },
+			{ type: 'NSArray', name: 'didUpdateLocations' }
+		],
+		action: handleNewPosition
+	})
+	.build();
+var locationDelegate = new LocDelegate();
+manager.delegate = locationDelegate;
+
+/*
+ We're all set. Go for it!
+ */
+manager.startUpdatingLocation();
+if (CLLocationManager.locationServicesEnabled()) {
+	label.text = 'RUN!'.toUTF8();
+	NSTimer.scheduledTimerWithTimeInterval(1 / TARGET_FPS, new TimerCallback(), NSSelectorFromString('update:'), null, true);
+}
+else {
+	label.text = 'Please enable GPS for this app!'.toUTF8();
+}
