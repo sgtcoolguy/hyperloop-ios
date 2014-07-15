cb2a6c25a67a88de297430a71e7f5a5dfcd3dc09
Kota Iguchi <developer@infosia.co.jp>
2014-07-15 17:55:59 +0900

    Add native implementation of instanceof

--- templates/hyperloop.cpp
+++ templates/hyperloop.cpp
@@ -38,3 +38,32 @@ EXPORTAPI void HyperloopNativeLogger(const char *str)
 	NSLog(@"%@",msg);
 	[msg release];
 }
+
+EXPORTAPI JSValueRef Hyperloop_Binary_InstanceOf(JSContextRef ctx, JSObjectRef function, JSObjectRef thisObject, size_t argumentCount, const JSValueRef arguments[], JSValueRef* exception) {
+    if (argumentCount < 2)
+    {
+        *exception = HyperloopMakeException(ctx, "Wrong arguments passed to IsInstanceOf");
+        return JSValueMakeUndefined(ctx);
+    }
+
+    auto isObject_a = JSValueIsObject(ctx, arguments[0]);
+    auto isObject_b = JSValueIsObject(ctx, arguments[1]);
+
+    if (isObject_a && isObject_b)
+    {
+        auto obj_a = JSValueTo_id(ctx, arguments[0], exception);
+        auto obj_b = JSValueTo_id(ctx, arguments[1], exception);
+
+        // nullptr means object is not a native object
+        if (obj_a == nullptr || obj_b == nullptr) {
+            return JSValueMakeBoolean(ctx, false);
+        }
+
+        return JSValueMakeBoolean(ctx, ([obj_a isKindOfClass:[obj_b class]]));
+    }
+    else if (!isObject_a && !isObject_b) {
+        return JSValueMakeBoolean(ctx, JSValueIsStrictEqual(ctx, arguments[0], arguments[1]));
+    }
+
+    return JSValueMakeBoolean(ctx, false);
+}
