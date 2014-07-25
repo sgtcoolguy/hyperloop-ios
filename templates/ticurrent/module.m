2a46fe4aec6563c674e5f68a98ff1671a9532a11
Pedro Enrique <pedro.tma@gmail.com>
2014-07-25 14:04:02 -0700

    Fixed destination path of packaged ti module

--- templates/ticurrent/module.m
+++ templates/ticurrent/module.m
@@ -32,96 +32,7 @@ extern TiStringRef kTiStringPropertyKey;
 extern TiStringRef kTiStringEventKey;
 extern TiStringRef kTiStringExportsKey;
 
-// Gets a JSValue and converts it to an Objective-C object
-// Most of this code is taken, and modified, from TiBindingTiValueToNSObject()
-static id JSValueToObjectC(KrollContext *context, TiValueRef objRef)
-{
-	if(objRef == NULL){
-		return nil;
-	}
-	TiType tt = TiValueGetType([context context], objRef);
-	switch (tt) {
-		case kJSTypeUndefined:{
-			return nil;
-		}
-		case kJSTypeNull: {
-			return [NSNull null];
-		}
-		case kJSTypeBoolean: {
-			return [NSNumber numberWithBool:TiValueToBoolean([context context], objRef)];
-		}
-		case kJSTypeNumber: {
-			double result = TiValueToNumber([context context], objRef, NULL);
-			if (std::isnan(result)) {
-				return [NSDecimalNumber numberWithDouble:result];
-			}
-			return [NSNumber numberWithDouble:result];
-		}
-		case kJSTypeString: {
-			TiStringRef stringRefValue = TiValueToStringCopy([context context], objRef, NULL);
-			NSString * result = [(NSString *)TiStringCopyCFString (kCFAllocatorDefault,stringRefValue) autorelease];
-			TiStringRelease(stringRefValue);
-			return result;
-		}
-		case kJSTypeObject: {
-			id privateObject = (id)HyperloopJSValueToVoidPointer([context context], objRef, NULL);
-			if (privateObject != nil) {
-				return privateObject;
-			}
-			TiObjectRef obj = TiValueToObject([context context], objRef, NULL);
-			if (TiValueIsArray([context context],obj)) {
-				TiValueRef length = TiObjectGetProperty([context context], obj, kTiStringLength, NULL);
-				double len = TiValueToNumber([context context], length, NULL);
-				NSMutableArray* resultArray = [[NSMutableArray alloc] initWithCapacity:len];
-				for (size_t c=0; c<len; ++c)
-				{
-					TiValueRef valueRef = TiObjectGetPropertyAtIndex([context context], obj, c, NULL);
-					id value = JSValueToObjectC(context,valueRef);
-					//TODO: This is a temprorary workaround for the time being. We have to properly take care of [undefined] objects.
-					if(value == nil){
-						[resultArray addObject:[NSNull null]];
-					}
-					else{
-						[resultArray addObject:value];
-					}
-				}
-				return [resultArray autorelease];
-			}
-			if (TiValueIsDate([context context], obj)) {
-				TiValueRef fn = TiObjectGetProperty([context context], obj, kTiStringGetTime, NULL);
-				TiObjectRef fnObj = TiValueToObject([context context], fn, NULL);
-				TiValueRef resultDate = TiObjectCallAsFunction([context context],fnObj,obj,0,NULL,NULL);
-				double value = TiValueToNumber([context context], resultDate, NULL);
-				return [NSDate dateWithTimeIntervalSince1970:value/1000]; // ms for JS, sec for Obj-C
-			}
-			if (TiObjectIsFunction([context context],obj)) {
-				return [[[KrollCallback alloc] initWithCallback:obj thisObject:TiContextGetGlobalObject([context context]) context: context] autorelease];
-			}
-			{
-				TiPropertyNameArrayRef props = TiObjectCopyPropertyNames([context context],obj);
-				NSMutableDictionary *dict = [[NSMutableDictionary alloc] init];
-				
-				size_t count = TiPropertyNameArrayGetCount(props);
-				for (size_t i = 0; i < count; i++)
-				{
-					TiStringRef jsString = TiPropertyNameArrayGetNameAtIndex(props, i);
-					TiValueRef v = TiObjectGetProperty([context context], obj, jsString, NULL);
-					NSString* jsonkey = (NSString *)TiStringCopyCFString(kCFAllocatorDefault, jsString);
-					id jsonvalue = JSValueToObjectC(context,v);
-					if (jsonvalue && jsonkey) {
-						[dict setObject:jsonvalue forKey:jsonkey];
-					}
-					[jsonkey release];
-				}
-				TiPropertyNameArrayRelease(props);
-				return [dict autorelease];
-			}
-		}
-		default: {
-			return nil;
-		}
-	}
-}
+
 
 static void TiObjectPropertyIterator(TiContextRef context, TiObjectRef object, void(^visitor)(NSString *property, TiStringRef propertyName, TiValueRef propertyValue))
 {
@@ -183,7 +94,7 @@ static void TiObjectPropertyIterator(TiContextRef context, TiObjectRef object, v
 	HyperloopInitialize_Source();
 
 	// run the hyperloop module in the global context of kroll
-	JSValueRef result = HyperloopModuleRequire(ctx,&exception,"<%=moduleid%>");
+	JSValueRef result = HyperloopModuleRequire(ctx,&exception,"/main");
 
 	if (result==NULL)
 	{
@@ -220,7 +131,7 @@ static void TiObjectPropertyIterator(TiContextRef context, TiObjectRef object, v
 
 	// copy all the exported properties from module into this module
 	TiObjectPropertyIterator(ctx,exports,^(NSString *property, TiStringRef propertyName, TiValueRef propertyValue){
-		[me replaceValue:JSValueToObjectC(kroll, propertyValue) forKey:property notification:NO];
+ 		[me replaceValue:TiValueToId(kroll,propertyValue) forKey:property notification:NO];	
 	});	
 	
 }
